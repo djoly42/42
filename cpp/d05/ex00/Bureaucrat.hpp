a5f489d5d42d72ec94d59f380b2f1d421b7e2ea6
djoly <you@example.com>
2017-10-10 18:10:48 +0200

    check cpp 05 00

--- cpp/d05/ex00/Bureaucrat.hpp
+++ cpp/d05/ex00/Bureaucrat.hpp
@@ -6,7 +6,7 @@
 /*   By: djoly <djoly@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/10/09 13:31:06 by djoly             #+#    #+#             */
-/*   Updated: 2017/10/10 13:36:24 by djoly            ###   ########.fr       */
+/*   Updated: 2017/10/10 18:07:37 by djoly            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,29 +19,29 @@ class Bureaucrat  {
 public:
   Bureaucrat(std::string name, int grade);
   Bureaucrat(Bureaucrat const & src);
-  virtual ~Bureaucrat(void);
+  virtual   ~Bureaucrat(void);
 
   
-  std::string getName(void)const;
-  int getGrade(void)const;
+  std::string   getName(void)const;
+  int           getGrade(void)const;
   
-  void gradePlus(void);
-  void gradeMoins(void);
+  void          gradePlus(void);
+  void          gradeMoins(void);
 
-  Bureaucrat& operator=(Bureaucrat const & rhs);
+  Bureaucrat&   operator=(Bureaucrat const & rhs);
   
-  class GradeTooHighException : public std::exception{
+  class         GradeTooHighException : public std::exception{
     public:
-      GradeTooHighException(std::string  error);
-      GradeTooHighException(GradeTooHighException const & src);      
-      virtual ~GradeTooHighException(void)throw();
-      virtual const char* what() const throw() ;
-      GradeTooHighException& operator=(GradeTooHighException const & rhs);
+        GradeTooHighException(std::string  error);
+        GradeTooHighException(GradeTooHighException const & src);      
+        virtual ~GradeTooHighException(void)throw();
+        virtual const char* what() const throw() ;
+        GradeTooHighException& operator=(GradeTooHighException const & rhs);
       
 
     private:
-      GradeTooHighException(void);    
-      std::string   _custoMsg;
+        GradeTooHighException(void);    
+        std::string   _custoMsg;
   };
   
   class GradeTooLowException : public std::exception{
