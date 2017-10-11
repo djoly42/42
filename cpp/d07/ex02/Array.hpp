05ae5cb41aece15408372ff90c79d7f28b25953d
djoly <you@example.com>
2017-10-11 18:18:55 +0200

    c[[ 07 02

--- cpp/d07/ex02/Array.hpp
+++ cpp/d07/ex02/Array.hpp
@@ -6,7 +6,7 @@
 /*   By: djoly <djoly@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/10/11 10:36:50 by djoly             #+#    #+#             */
-/*   Updated: 2017/10/11 16:23:33 by djoly            ###   ########.fr       */
+/*   Updated: 2017/10/11 18:18:10 by djoly            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,37 +17,47 @@
 
 #include <iostream>
 
-template <typename U>
+template <typename T>
 class Array {
 public:
-    U            *tab;
+    T            *tab;
+    bool            _b;
     
     Array(void){
         std::cout << "call constructor void" << std::endl;
-        
-       this->tab = new U();
+        /*if (this->isConst(this)){
+            std::cout << "is const" <<std::endl;
+           
+        }*/
+       this->tab = new T[0]();
        this->_len = 0;
-   };
+    };
 
     Array(unsigned int n){
         std::cout << "call constructor n" << std::endl;
-        
+        /*if (this->isConst(this)){
+            std::cout << "is const" <<std::endl;
+            
+        }*/
         if(n == 0){
             Array();
         }
         else{       
         this->_len = n;
-        this->tab = new U[n]();
-        
-    }
-   };
+        this->tab = new T[n]();
+        }
+    };
 
     Array(Array const & src){
+        /*if (this->isConst(this)){
+            std::cout << "is const" <<std::endl;
+            
+        }*/
         std::cout << "call constructor src" << std::endl;
         
-        this->tab = new U[src.getLen()];
-        this->_len = src.getLen();
-        for(unsigned int i; i < this->_len; i++){
+        this->tab = new T[src.size()];
+        this->_len = src.size();
+        for(unsigned int i = 0; i < this->_len; i++){
             this->tab[i] = src.tab[i];
         }
         
@@ -55,11 +65,26 @@ public:
     };
    
     virtual ~Array(void){
-        std::cout << "call destructor " << std::endl;
-
+        //std::cout << "call destructor " << std::endl;
+    
         delete [] this->tab ;
-        std::cout << "end destructor " << std::endl;
+        //std::cout << "end destructor " << std::endl<< std::endl;
     };
+/*
+    bool isConst(Array& x)
+    {
+        (void)x;
+        this->_b = true;
+        return false;
+    }
+    
+    bool isConst(Array const& x)
+    {
+        (void)x;
+        this->_b = true;
+        return true;
+    }
+  */  
 
     unsigned int     size(void) const{
         return this->_len;
@@ -67,7 +92,15 @@ public:
     };
 
     Array& operator=(Array const & rhs){
-        this->tab = new U[rhs.size()];
+    
+
+        if (this->_len == 0){
+            delete this->tab;
+        }
+        else
+            delete [] this->tab;
+
+        this->tab = new T[rhs.size()];
         this->_len = rhs.size();
         for(unsigned int i = 0; i < this->_len; i++){
             this->tab[i] = rhs.tab[i];
@@ -75,7 +108,10 @@ public:
         return *this;
     };
   
-    U& operator[](unsigned int i){
+    T& operator[](unsigned int i)const{
+        /*if( this->_b )
+            std::cout << " IS CONST " << std::endl;
+        */
         if (i >= _len){
             throw std::exception();
         }
@@ -85,14 +121,17 @@ public:
 
 private:
     unsigned int    _len;
+ 
 
 };
-/*
-std::ostream &      operator<< (std::ofstream & o, Array<T> const & rhs ){
+
+template <typename T >
+std::ostream &      operator<< (std::ostream & o, Array<T> const & rhs ){
+    o << "tab size: ";
     for (unsigned int i = 0; i < rhs.size() ; i++)
-        o << "tab["<< i<<"]: " << rhs[i] << " ";
+        o << " tab["<< i<<"]: " << rhs[i] << " ";
     o << std::endl;
-    return 0;
-}*/
+    return o;
+}
 
 #endif
--- cpp/d07/ex02/main.cpp
+++ cpp/d07/ex02/main.cpp
@@ -6,121 +6,152 @@
 /*   By: djoly <djoly@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/10/11 11:22:22 by djoly             #+#    #+#             */
-/*   Updated: 2017/10/11 16:27:15 by djoly            ###   ########.fr       */
+/*   Updated: 2017/10/11 18:16:43 by djoly            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include "Array.hpp"
 
+int main(void)
+{
 
-int     main(void){
-
-
-    std::cout << "1/ ---" << std::endl;
+    std::cout << "1/ --- int[5] empty" << std::endl;
+    try
     {
         Array<int> ar(5);
-        std::cout << "size: " <<ar.size() << std::endl;
-        try {
-            for (unsigned int i = 0; i < ar.size() ; i++)
-            std::cout << "tab["<< i<<"]: " << ar[i] << " ";
-        std::cout << std::endl;
-        }
-        catch (std::exception & e){
-            std::cout << e.what() << std::endl;
-        }
-        std::cout << std::endl;
-        
+        std::cout << ar << std::endl;
     }
+    catch (std::exception &e)
+    {
+        std::cout << e.what() << std::endl;
+    }
+    std::cout << std::endl;
 
-
-    std::cout << "2/ ---" << std::endl;
+    std::cout << "2/ --- int empty" << std::endl;
+    try
     {
         Array<int> ar;
-        std::cout << "size: " <<ar.size() << std::endl;
-        try {
-            for (unsigned int i = 0; i < ar.size() ; i++)
-            std::cout << "tab["<< i<<"]: " << ar[i] << " ";
-        std::cout << std::endl;
-        }
-        catch (std::exception & e){
-            std::cout << e.what() << std::endl;
-        }
-        std::cout << std::endl;
-        
+        std::cout << ar << std::endl;
     }
+    catch (std::exception &e)
+    {
+        std::cout << e.what() << std::endl;
+    }
+    std::cout << std::endl;
 
+    std::cout << "3/ --- int[5] fill" << std::endl;
 
-    std::cout << "3/ ---" << std::endl;
+    try
     {
         Array<int> ar(5);
-        
+
         ar[0] = 0;
         ar[1] = 10;
         ar[2] = 20;
         ar[3] = 30;
-     
-        std::cout << "size: " <<ar.size() << std::endl;
-        try {
-            for (unsigned int i = 0; i < ar.size() ; i++)
-                std::cout << "tab["<< i<<"]: " << ar[i] << " ";
-        std::cout << std::endl;
-        }
-        catch (std::exception & e){
-            std::cout << e.what() << std::endl;
-        }
-        std::cout << std::endl;
-        
+        ar[4] = 40;
+        std::cout << ar << std::endl;
     }
-    std::cout << "4/ ---" << std::endl;
+    catch (std::exception &e)
+    {
+        std::cout << e.what() << std::endl;
+    }
+    std::cout << std::endl;
+
+    std::cout << "4/ ---string[5] vide" << std::endl;
+
+    try
     {
         Array<std::string> ar(5);
-        std::cout << "size: " <<ar.size() << std::endl;
-        try {
-            for (unsigned int i = 0; i < ar.size() ; i++)
-            std::cout << "tab["<< i<<"]: " << ar[i];
-        std::cout << std::endl;
-        }
-        catch (std::exception & e){
-            std::cout << e.what() << std::endl;
-        }
-        std::cout << std::endl;
-        
+        std::cout << ar << std::endl;
     }
+    catch (std::exception &e)
+    {
+        std::cout << e.what() << std::endl;
+    }
+    std::cout << std::endl;
 
-    std::cout << "5/ ---" << std::endl;
+    std::cout << "5/ --- string[5] fill" << std::endl;
+
+    try
     {
         Array<std::string> ar(3);
         ar[0] = "abc";
         ar[1] = "def";
         ar[2] = "ghi";
-        std::cout << "size: " <<ar.size() << std::endl;
-        try {
-            for (unsigned int i = 0; i < ar.size() ; i++)
-            std::cout << "tab["<< i<<"]: " << ar[i] << " ";
-        std::cout << std::endl;
-        }
-        catch (std::exception & e){
-            std::cout << e.what() << std::endl;
-        }
-        std::cout << std::endl;
-        
-    }
 
-    std::cout << "6/ ---" << std::endl;
+        std::cout << ar << std::endl;
+    }
+    catch (std::exception &e)
+    {
+        std::cout << e.what() << std::endl;
+    }
+    std::cout << std::endl;
+
+    std::cout << "6/ --- string[] empty" << std::endl;
+
+    try
     {
         Array<std::string> ar;
-        std::cout << "size: " <<ar.size() << std::endl;
-        try {
-            for (unsigned int i = 0; i < ar.size() ; i++)
-            std::cout << "tab["<< i<<"]: " << ar[i] << " ";
-        std::cout << std::endl;
+        std::cout << ar << std::endl;
+    }
+    catch (std::exception &e)
+    {
+        std::cout << e.what() << std::endl;
+    }
+    std::cout << std::endl;
+
+    std::cout << "7/ --- int[5] fill" << std::endl;
+
+    try
+    {
+        Array<int> ar(5);
+        ar[0] = 0;
+        ar[1] = 10;
+        ar[2] = 20;
+        ar[3] = 30;
+        ar[4] = 40;
+
+        Array<int> ar10(2);
+        ar10 = ar;
+        Array<int> ar2 = ar;
+        Array<int> ar3(ar);
+        std::cout << ar;
+        std::cout << ar2;
+        std::cout << ar3;
+        std::cout << ar10;
+    }
+    catch (std::exception &e)
+    {
+        std::cout << e.what() << std::endl;
+    }
+    std::cout << std::endl;
+
+    std::cout << "8/ --- int[5] fill" << std::endl;
+    
+        try
+        {
+            Array<int> ar(5);
+            ar[0] = 0;
+            ar[1] = 10;
+            ar[2] = 20;
+            ar[3] = 30;
+            ar[4] = 40;
+    
+            Array<int> ar10(2);
+            //ar = ar10;
+            Array<int> ar2 = ar;
+            Array<int> ar3(ar);
+            std::cout << ar;
+            std::cout << ar2;
+            std::cout << ar3;
+            std::cout << ar10;
         }
-        catch (std::exception & e){
+        catch (std::exception &e)
+        {
             std::cout << e.what() << std::endl;
         }
         std::cout << std::endl;
-        
-    }
     return 0;
 }
